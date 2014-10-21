8fb3cfd40d0ecb40c57d91eea42d614e50d344b0
tim <tim@magnetic.io>
2014-10-21 09:56:31 +0200

    Adds datapoint

---
+++ app/models/kairosdb/DataPoint.scala
@@ -0,0 +1,14 @@
+package models.kairosdb
+
+import play.api.libs.json.Json
+
+
+case class DataPoint( name: String, timestamp: Long, value: Int, tags: Option[Map[String, String]])
+
+object DataPointJson {
+
+  // Json reading/writing
+  implicit val datapointReads = Json.reads[DataPoint]
+  implicit val datapointWrites = Json.writes[DataPoint]
+
+}
